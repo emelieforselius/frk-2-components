c935fd8f861b1a16153c4493024723fd0972818a
Emelie Forselius <emelieforselius@outlook.com>
2024-09-16 14:05:13 +0200

    Login established

---
+++ src/stories/components/Login/Login.stories.jsx
@@ -0,0 +1,11 @@
+import Login from "./Login";
+
+export default {
+    title: 'FRK/Login',
+    component: Login
+}
+
+export const Default = {
+
+
+}
---
+++ src/stories/components/Login/Password.stories.jsx
@@ -0,0 +1,11 @@
+import Password from "./Password"
+
+export default {
+    title: 'FRK/Password',
+    component: Password
+}
+
+export const Default = {
+
+
+}
